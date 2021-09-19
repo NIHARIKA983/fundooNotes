7875971f7df3a6700e8fe43a5036c258402ace0a
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-09-19 17:26:28 +0530

    Refactor:Validation for Registration

--- app/utilities/validation.js
+++ app/utilities/validation.js
@@ -10,8 +10,7 @@ class Validation {
 
             lastName: Joi.string()
                 .min(2)
-                .required()
-                .pattern(new RegExp('^[A-Z]{1}[a-z]{1,}$')),
+                .required(),
 
             email: Joi.string()
                 .pattern(new RegExp('^[a-zA-z]{3}([+-_ .]*[a-zA-Z0-9]+)*[@][a-zA-z0-9]+(.[a-z]{2,3})*$'))
